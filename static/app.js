a955b2811fc6865069ce43be1f66e68e45c535f4
AkshayM2001 <akshaymatale2001@gmail.com>
2023-12-31 20:02:46 +0530

    STARTED showcase

--- static/app.js
+++ static/app.js
@@ -31,6 +31,35 @@ var swiperCategories = new Swiper('.categories__container', {
 
 /* ================ SWIPER CATEGORIES ================= */
 
+var swiperProducts = new Swiper('.new__container', {
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          "@0.75": {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          "@1.00": {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          "@1.50": {
+            slidesPerView: 4,
+            spaceBetween: 24,
+          }
+
+    },
+});
+
+
 /* ================ PRODUCT TABS ================= */
 const tabs = document.querySelectorAll('[data-target]'),
 tabContent = document.querySelectorAll('[content]');
